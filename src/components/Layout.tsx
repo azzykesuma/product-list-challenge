91d4b70bbe09487c19dfb0aaa7402650c4ba2a46
Azzzy <official.azzy@gmail.com>
2025-02-04 11:31:25 +0700

    changing bg color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 const Layout = ({children} : PropsWithChildren) => {
   return (
-    <main className='h-screen w-full p-4 px-6 bg-rose-200 overflow-hidden md:p-14 lg:p-20'>
+    <main className='h-screen w-full p-4 px-6 bg-rose-50 overflow-hidden md:p-14 lg:p-20'>
         {children}
     </main>
   )
